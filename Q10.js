6dea61c93179a34968b78bbfa3b6d3ce920b6cdb
sankyk2020 <69984129+sanky2020@users.noreply.github.com>
2021-10-10 12:44:22 +0530

    Create Q10.js

---
+++ Q10.js
@@ -0,0 +1,8 @@
+//How can a given string be reversed using recursion?
+function reverseString(str) {
+if (str === "") // This is the terminal case that will end the recursion
+return "";
+else
+return reverseString(str.substr(1)) + str.charAt(0);
+}
+console.log(reverseString("hello"));
