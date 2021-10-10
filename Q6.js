52d84bc6ed5ac1112653c870924ed90fee97cff5
sankyk2020 <69984129+sanky2020@users.noreply.github.com>
2021-10-10 12:41:57 +0530

    Create Q6.js

---
+++ Q6.js
@@ -0,0 +1,5 @@
+//How do you reverse an array in place in JavaScript? In place means you cannot
+//create a new array. You have to update the original array.
+let yourArray = [1,2,3,4]
+let reverseArray = yourArray.slice().reverse()
+console.log(reverseArray)
