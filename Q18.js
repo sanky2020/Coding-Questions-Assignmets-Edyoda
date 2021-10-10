190f4e2f0c53445062a8e9df6a68b1eeb98da9cf
sankyk2020 <69984129+sanky2020@users.noreply.github.com>
2021-10-10 12:49:47 +0530

    Create Q18.js

---
+++ Q18.js
@@ -0,0 +1,14 @@
+//How do you check if a given string is a palindrome ?
+function checkPalindrome(str) {
+const arrayValues = string.split('');
+const reverseArrayValues = arrayValues.reverse();
+const reverseString = reverseArrayValues.join('');
+if(string == reverseString) {
+console.log('It is a palindrome');
+}
+else {
+console.log('It is not a palindrome');
+}
+}
+const string = prompt('Enter a string : ');
+checkPalindrome(string);
